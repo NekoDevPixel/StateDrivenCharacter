14088c3ee31d60c3d4b44e3842eeb9553c01283d
LEE_SANG_JUN <sahwang04@gmail.com>
2025-04-18 00:18:29 +0900

    달리기 애니메이션 & 점프  애니메이션
    기본적인 캐릭터의 좌우 움직임, 달리기 애니메이션, 점프 애니메이션 구현

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,16 +1,69 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rb;
+    public float speed = 5f;
+    public float jump = 4f;
+    Vector2 InputVec2;
+    private bool jumpCheck;
+    private Animator animator;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rb.linearVelocity = new Vector2(InputVec2.x * speed, rb.linearVelocity.y);
+        LeftandRight();
+        animator.SetFloat("run", Mathf.Abs(InputVec2.x));//변화하는 x좌표의 절대값을 인스펙터에서 비교
+    }
+
+    void OnMove(InputValue value){
+        InputVec2 = value.Get<Vector2>();
+    }
+    
+    void OnJump(){
+        if(jumpCheck){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
+            animator.SetBool("jump",true);
+        }
         
     }
+
+    void LeftandRight(){
+        if (InputVec2.x > 0){
+            rb.transform.localScale = new Vector3(1,1);
+        }
+        else if(InputVec2.x < 0){
+            rb.transform.localScale = new Vector3(-1,1);
+        }
+    }
+    //중복 점프 방지
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // 지면에 닿았을 때
+        {
+            jumpCheck = true; // 지면에 닿았다고 설정
+            animator.SetBool("jump", false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // 지면에 닿았을 때
+        {
+            jumpCheck = false; // 지면과 멀어졌을떄떄
+        }
+    }
+
+    // void jumpAnim(){
+    //     if()
+    //     animator.SetBool("jump",true);
+    // }
 }
